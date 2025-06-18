2c4d535e445f123e730ad257ad76ce4a045d6dbc
2018csb1082 <daksh1sharma1@gmail.com>
2025-06-18 12:16:26 +0530

    Added Form Logic and CSS

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -3,13 +3,46 @@ import {useState} from 'react';
 import './SignUpForm.css'
 
 function SignUpForm({onLogin}) {
-  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const handleMailChange = (event) => {
+    setEmail(event.target.value);
+  }
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+  const handleSubmit = () => {
+    //check mail and password
+    onLogin();
+  };
+
   return (
     <div className='SignUpForm'>
-      SignUp
+      <div className='FormRow'>
+        <h1>Sign Up</h1>
+      </div>
+      <div className='FormRow'>
+        <input 
+          type="email"
+          className='FormInput'
+          placeholder='Email'
+          value={email}
+          onChange={handleMailChange}
+        />
+      </div>
+      <div className='FormRow'>
+        <input 
+          type="Password"
+          className='FormInput'
+          placeholder='Password'
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </div>
+      <div className='FormRow'>
+        <button className='FormButton' onClick={handleSubmit}>Sign Up</button>
+      </div>
     </div>
   );
 }
