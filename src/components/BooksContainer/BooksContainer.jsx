aa08dfddeb1e95aa2fa653ce5a2c2f40c6006fbc
2018csb1082 <daksh1sharma1@gmail.com>
2025-06-23 20:02:59 +0530

    Added fetching dummy books and loading

--- src/components/BooksContainer/BooksContainer.jsx
+++ src/components/BooksContainer/BooksContainer.jsx
@@ -1,18 +1,57 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { BeatLoader } from "react-spinners";
 
 import BookOptions from '../BookOptions/BookOptions';
 import './BooksContainer.css';
 
 function BooksContainer () {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   const options = ["Reading", "Wishlist", "Completed"];
   const [selection, setSelection] = useState(options[0]);
   const selectOption = (option) => {
     setSelection(option);
   }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/books.json');
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    setLoading(true);
+    fetchData();
+  }, [selection]);
+
   return (
     <div className="BooksContainer">
       <BookOptions options={options} selection={selection} selectOption={selectOption}/>
-      Books
+      {loading || error ? 
+        <div className="TempContainer">
+          {loading ?
+            <BeatLoader
+              color="#4169E1"
+              size={35}
+              aria-label="BeatLoader"
+              data-testid="BeatLoader"
+            /> :
+            error}
+        </div> :
+        <div className="BookListContainer">
+          {JSON.stringify(data)}
+        </div>
+      }
     </div>
   );
 }
